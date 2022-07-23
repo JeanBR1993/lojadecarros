a5caaedc9382243451f00d33e6aa5bbe72a13dfa
Jean <jeanmabe1993@gmail.com>
2022-07-23 00:58:11 -0300

    new file:   carros/# WebScrapper dos dados da tabela fipe.py 	new file:   carros/__init__.py 	new file:   carros/admin.py 	new file:   carros/apps.py 	new file:   carros/migrations/__init__.py 	new file:   carros/models.py 	new file:   carros/tests.py 	new file:   carros/views.py 	new file:   db.sqlite3 	new file:   lojadecarros/__pycache__/__init__.cpython-310.pyc 	new file:   lojadecarros/__pycache__/settings.cpython-310.pyc 	new file:   lojadecarros/__pycache__/urls.cpython-310.pyc 	new file:   lojadecarros/__pycache__/wsgi.cpython-310.pyc

---
+++ carros/# WebScrapper dos dados da tabela fipe.py
@@ -0,0 +1,1 @@
+# WebScrapper dos dados da tabela fipe
---
+++ carros/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+class Fabricante():
+    fabricante = [
+        ('Chevrolet/GM', 'Chevrolet/GM'),
+        ('Ford', 'Ford'),
+        ('Peugeot', 'Peugeot'),
+        ('Citroen', 'Citroen'),
+        ('Volkswagen', 'Volkswagen'),
+        ('Fiat', 'Fiat'),
+        ('Ferrari', 'Ferrari'),
+        ('Mercedes-Benz', 'Mercedes-Benz'),
+        ('Volvo', 'Volvo'),
+        ('BMW', 'BMW'),
+        ('Mini', 'Mini'),
+        ('Chrysler', 'Chrysler'),
+        ('Jeep', 'Jeep'),
+        ('Hyundai', 'Hyundai'),
+        ('Toyota', 'Toyota'),
+        ('Honda', 'Honda'),
+        ('Mitsubishi', 'Mitsubishi'),
+        ('Subaru', 'Subaru'),
+        ('Tesla', 'Tesla'),
+        ('Jaguar', 'Jaguar'),
+        ('Porsche', 'Porsche'),
+        ('Puma', 'Puma'),
+        ('Nissan', 'Nissan'),
+        ('Lamborghini', 'Lamborghini'),
+        ('Land Rover', 'Land Rover'),
+        ('Kawazaki', 'Kawazaki'),
+        ('Kia', 'Kia'),
+        ('Lexus', 'Lexus'),
+        ('Mazda', 'Mazda'),
+        ('Dodge', 'Dodge'),
+        ('Peugeot', 'Peugeot'),
+        ('Ssangyong', 'Ssangyong'),
+        ('Audi', 'Audi'),
+        ('Smart', 'Smart'),
+        ('Jac', 'Jac'),
+        ('Daewoo', 'Daewoo'),
+        ('Bentley', 'Bentley'),
+        ('Aston Martin', 'Aston Martin'),
+        ('Alfa Romeo', 'Alfa Romeo'),
+    ]
+
+class Carros(models.Model):
+    modelo = 
+
